21daba876e8d20567ea1298591082829285d5224
jojaeng2 <ds4ouj@naver.com>
2023-01-22 13:10:48 +0900

    feat : 호출한 메서드에서 MDC 호출해도 성공확인

--- example/src/main/java/logstash/example/controller/LogStashController.java
+++ example/src/main/java/logstash/example/controller/LogStashController.java
@@ -2,6 +2,8 @@ package logstash.example.controller;
 
 
 import logstash.example.controller.dto.LogDto;
+import logstash.example.controller.service.LogStashService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
@@ -11,15 +13,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class LogStashController {
+
+  private final LogStashService logStashService;
 
   @GetMapping("/hello")
   public ResponseEntity getHello() {
     LogDto logDto = new LogDto("name", "id");
     MDC.put("hello1", "world!!!!!!!!!!!!!!!!!!");
     MDC.put("hello2", "world!!!!!!!!!!!!!!!!!!");
-    MDC.put("hello3", "world!!!!!!!!!!!!!!!!!!");
-
+    logStashService.call();
     log.info(logDto.toString());
     MDC.put("hello4", "world!!!!!!!!!!!!!!!!!!");
     MDC.put("hello5", "world!!!!!!!!!!!!!!!!!!");
---
+++ example/src/main/java/logstash/example/controller/service/LogStashService.java
@@ -0,0 +1,12 @@
+package logstash.example.controller.service;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogStashService {
+
+
+  public void call() {
+  }
+}
